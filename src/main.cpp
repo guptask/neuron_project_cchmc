0cb2919eb357418799489d8c8e80a4d7c82abb80
Sounak Gupta <sounak.besu@gmail.com>
2014-10-08 16:53:55 -0400

    massive modification - merged few z layers to create a clearer image.

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,112 @@
 #include <memory>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <vector>
 
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/imgcodecs.hpp"
 
-#include "Moments.hpp"
+#define NUM_Z_LAYERS  3  // Merge a certain number of z layers
 
-int main (int argc, char *argv[]) {
 
-    DIR *read_dir = opendir(argv[1]);
+enum class ChannelType : unsigned char {
+    BLUE = 0,
+    GREEN,
+    RED
+};
+
+/* Enhance the image */
+bool enhanceImage(cv::Mat src, ChannelType channel_type, cv::Mat *dst) {
+
+    // Convert to grayscale and equalize the histogram
+    cv::Mat src_equalized;
+    cvtColor (src, src_equalized, cv::COLOR_BGR2GRAY);
+    equalizeHist(src_equalized, src_equalized);
+
+    // Enhance the image using Gaussian blur and thresholding
+    cv::Mat enhanced;
+    switch(channel_type) {
+        case ChannelType::BLUE: {
+            cv::GaussianBlur(src_equalized, enhanced, cv::Size(5,5), 0, 0);
+            cv::threshold(enhanced, enhanced, 218, 255, cv::THRESH_BINARY);
+        } break;
+
+        case ChannelType::GREEN: {
+            cv::GaussianBlur(src_equalized, enhanced, cv::Size(25,25), 0, 0);
+            cv::threshold(enhanced, enhanced, 208, 255, cv::THRESH_BINARY);
+        } break;
+
+        case ChannelType::RED: {
+            cv::GaussianBlur(src_equalized, enhanced, cv::Size(25,25), 0, 0);
+            cv::threshold(enhanced, enhanced, 208, 255, cv::THRESH_BINARY);
+        } break;
+
+        default: {
+            std::cerr << "Invalid channel type" << std::endl;
+            return false;
+        }
+    }
+    *dst = enhanced;
+    return true;
+}
+
+/* Find the contours in the image */
+void contourCalc(cv::Mat src, cv::Mat *dst) {
+
+    // Blur the image
+    cv::Mat src_blur;
+    cv::blur(src, src_blur, cv::Size(3,3) );
+
+    cv::Mat canny_output;
+    std::vector<std::vector<cv::Point>> contours;
+    std::vector<cv::Vec4i> hierarchy;
+
+    // Detect edges using canny
+    cv::Canny(src_blur, canny_output, 0, 255, 3);
+
+    // Find contours
+    cv::findContours(canny_output, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
+
+    // Get the moments
+    std::vector<cv::Moments> mu(contours.size());
+    for (size_t i = 0; i < contours.size(); i++) {
+        mu[i] = cv::moments(contours[i], false);
+    }
+
+    // Get the mass centers:
+    std::vector<cv::Point2f> mc(contours.size());
+    for (size_t i = 0; i < contours.size(); i++) {
+        mc[i] = cv::Point2f(static_cast<float>(mu[i].m10/mu[i].m00), 
+                            static_cast<float>(mu[i].m01/mu[i].m00));
+    }
+
+    // Draw contours
+    cv::Mat drawing = cv::Mat::zeros(canny_output.size(), CV_8UC3);
+    cv::RNG rng(12345);
+    for (size_t i = 0; i< contours.size(); i++) {
+        cv::Scalar color = cv::Scalar(rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255));
+        cv::drawContours(drawing, contours, (int)i, color, 2, 8, hierarchy, 0, cv::Point());
+        cv::circle(drawing, mc[i], 4, color, -1, 8, 0);
+    }
+    *dst = drawing;
+
+    // Calculate the area with the moments 00 and compare with the result of the OpenCV function
+    /*for( size_t i = 0; i< contours.size(); i++ ) {
+        printf(" * Contour[%d] - Area (M_00) = %.2f - Area OpenCV: %.2f - 
+            Length: %.2f \n", (int)i, mu[i].m00, contourArea(contours[i]), arcLength( contours[i], true ) );
+        Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+        drawContours( drawing, contours, (int)i, color, 2, 8, hierarchy, 0, Point() );
+        circle( drawing, mc[i], 4, color, -1, 8, 0 );
+    }*/
+}
+
+/* Process the images inside each directory */
+bool processDir(std::string dir_name) {
+
+    DIR *read_dir = opendir(dir_name.c_str());
     if (!read_dir) {
-        std::cerr << "Could not open directory '" << argv[1] << "'" << std::endl;
-        return 0;
+        std::cerr << "Could not open directory '" << dir_name << "'" << std::endl;
+        return false;
     }
 
     // Count the number of images
@@ -28,9 +122,13 @@ int main (int argc, char *argv[]) {
     }
     closedir(read_dir);
 
+    if (z_count < NUM_Z_LAYERS) {
+        std::cerr << "Not enough z layers in the image." << std::endl;
+        return false;
+    }
+
     // Extract the input directory name
-    std::string str(argv[1]);
-    std::istringstream iss(str);
+    std::istringstream iss(dir_name);
     std::string token;
     getline(iss, token, '/');
     getline(iss, token, '/');
@@ -42,78 +140,92 @@ int main (int argc, char *argv[]) {
         mkdir(out_directory.c_str(), 0700);
     }
 
-    // Create the moments
-    std::unique_ptr<Moments> moments = std::unique_ptr<Moments>(new Moments());
-
+    std::vector<cv::Mat> blue(NUM_Z_LAYERS), green(NUM_Z_LAYERS), red(NUM_Z_LAYERS);
     for (uint8_t z_index = 1; z_index <= z_count; z_index++) {
 
         // Create the input filename and rgb stream output filenames
-        std::string in_filename, out_red_filename, out_green_filename, out_blue_filename;
+        std::string in_filename;
 
         if (z_index < 10) {
-            in_filename  = str + token + "_z0" + std::to_string(z_index) + "c1+2+3.tif";
-            out_red_filename   = out_directory + "z0" + std::to_string(z_index) + "_red.tif";
-            out_green_filename = out_directory + "z0" + std::to_string(z_index) + "_green.tif";
-            out_blue_filename  = out_directory + "z0" + std::to_string(z_index) + "_blue.tif";
-
+            in_filename  = dir_name + token + "_z0" + std::to_string(z_index) + "c1+2+3.tif";
         } else if (z_index < 100) {
-            in_filename  = str + token + "_z" + std::to_string(z_index) + "c1+2+3.tif";
-            out_red_filename   = out_directory + "z" + std::to_string(z_index) + "_red.tif";
-            out_green_filename = out_directory + "z" + std::to_string(z_index) + "_green.tif";
-            out_blue_filename  = out_directory + "z" + std::to_string(z_index) + "_blue.tif";
-
+            in_filename  = dir_name + token + "_z" + std::to_string(z_index) + "c1+2+3.tif";
         } else { // assuming number of z plane layers will never exceed 99
-            std::cerr << "There should not be that many z layers" << std::endl;
-            return 0;
+            std::cerr << "Does not support more than 99 z layers curently" << std::endl;
+            return false;
         }
 
         // Extract the bgr streams for each input image
         cv::Mat img = cv::imread(in_filename.c_str());
         if (img.empty()) {
             std::cerr << "Invalid input filename" << std::endl;
-            return 0;
+            return false;
         }
 
         std::vector<cv::Mat> channel(3);
         cv::split(img, channel);
 
-        cv::imwrite(out_blue_filename.c_str(), channel[0]);
-        cv::imwrite(out_green_filename.c_str(), channel[1]);
-        cv::imwrite(out_red_filename.c_str(), channel[2]);
+        blue[(z_index-1)%NUM_Z_LAYERS] = channel[0];
+        green[(z_index-1)%NUM_Z_LAYERS] = channel[1];
+        red[(z_index-1)%NUM_Z_LAYERS] = channel[2];
 
-        // Equalize the histogram
-        equalizeHist(channel[0], channel[0]);
-        equalizeHist(channel[1], channel[1]);
-        equalizeHist(channel[2], channel[2]);
+        // Merge, enhance, segment and extract features for a certain number of Z layers
+        if (z_index >= NUM_Z_LAYERS) {
 
-        // Enhance the image using Gaussian blur and thresholding
-        std::vector<cv::Mat> enhanced(3);
-        cv::GaussianBlur(channel[0], enhanced[0], cv::Size(5,5), 0, 0);
-        cv::threshold(enhanced[0], enhanced[0], 218, 255, cv::THRESH_BINARY);
-        cv::GaussianBlur(channel[1], enhanced[1], cv::Size(25,25), 0, 0);
-        cv::threshold(enhanced[1], enhanced[1], 208, 255, cv::THRESH_BINARY);
-        cv::GaussianBlur(channel[2], enhanced[2], cv::Size(25,25), 0, 0);
-        cv::threshold(enhanced[2], enhanced[2], 208, 255, cv::THRESH_BINARY);
+            // Blue channel
+            cv::Mat blue_merge, blue_enhanced, blue_contour;
+            cv::merge(blue, blue_merge);
+            std::string out_blue = out_directory + "z" + std::to_string(z_index-NUM_Z_LAYERS+1) + 
+                                            "_blue_" + std::to_string(NUM_Z_LAYERS) + "layers.tif";
+            cv::imwrite(out_blue.c_str(), blue_merge);
+            if(!enhanceImage(blue_merge, ChannelType::BLUE, &blue_enhanced)) {
+                return false;
+            }
+            out_blue.insert(out_blue.find_first_of("."), "_enhanced", 9);
+            cv::imwrite(out_blue.c_str(), blue_enhanced);
+            contourCalc(blue_enhanced, &blue_contour);
+            out_blue.insert(out_blue.find_first_of("."), "_contour", 8);
+            cv::imwrite(out_blue.c_str(), blue_contour);
 
-        out_blue_filename.insert(out_blue_filename.find_first_of("."), "_enhanced", 9);
-        cv::imwrite(out_blue_filename.c_str(), enhanced[0]);
-        out_green_filename.insert(out_green_filename.find_first_of("."), "_enhanced", 9);
-        cv::imwrite(out_green_filename.c_str(), enhanced[1]);
-        out_red_filename.insert(out_red_filename.find_first_of("."), "_enhanced", 9);
-        cv::imwrite(out_red_filename.c_str(), enhanced[2]);
+            // Green channel
+            cv::Mat green_merge, green_enhanced, green_contour;
+            cv::merge(green, green_merge);
+            std::string out_green = out_directory + "z" + std::to_string(z_index-NUM_Z_LAYERS+1) + 
+                                            "_green_" + std::to_string(NUM_Z_LAYERS) + "layers.tif";
+            cv::imwrite(out_green.c_str(), green_merge);
+            if(!enhanceImage(green_merge, ChannelType::GREEN, &green_enhanced)) {
+                return false;
+            }
+            out_green.insert(out_green.find_first_of("."), "_enhanced", 9);
+            cv::imwrite(out_green.c_str(), green_enhanced);
+            contourCalc(green_enhanced, &green_contour);
+            out_green.insert(out_green.find_first_of("."), "_contour", 8);
+            cv::imwrite(out_green.c_str(), green_contour);
 
-        // Moments calculation
-        std::vector<cv::Mat> result(3);
-        moments->apply(enhanced[0], &result[0], 0);
-        moments->apply(enhanced[1], &result[1], 0);
-        moments->apply(enhanced[2], &result[2], 0);
+            // Red channel
+            cv::Mat red_merge, red_enhanced, red_contour;
+            cv::merge(red, red_merge);
+            std::string out_red = out_directory + "z" + std::to_string(z_index-NUM_Z_LAYERS+1) + 
+                                                "_red_" + std::to_string(NUM_Z_LAYERS) + "layers.tif";
+            cv::imwrite(out_red.c_str(), red_merge);
+            if(!enhanceImage(red_merge, ChannelType::RED, &red_enhanced)) {
+                return false;
+            }
+            out_red.insert(out_red.find_first_of("."), "_enhanced", 9);
+            cv::imwrite(out_red.c_str(), red_enhanced);
+            contourCalc(red_enhanced, &red_contour);
+            out_red.insert(out_red.find_first_of("."), "_contour", 8);
+            cv::imwrite(out_red.c_str(), red_contour);
+        }
+    }
+    return true;
+}
 
-        out_blue_filename.insert(out_blue_filename.find_first_of("."), "_contour", 8);
-        cv::imwrite(out_blue_filename.c_str(), result[0]);
-        out_green_filename.insert(out_green_filename.find_first_of("."), "_contour", 8);
-        cv::imwrite(out_green_filename.c_str(), result[1]);
-        out_red_filename.insert(out_red_filename.find_first_of("."), "_contour", 8);
-        cv::imwrite(out_red_filename.c_str(), result[2]);
+int main(int argc, char *argv[]) {
+    std::string str(argv[1]);
+    if (!processDir(str)) {
+        return -1;
     }
     return 0;
 }
+
